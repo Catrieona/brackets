a5d5816f19120a974f01576f1c0811ca4c8c2ce8
Kate Makarenka <polumna-la@mail.ru>
2019-02-28 22:26:24 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  if(str.length%2 != 0){
+    return false
+   };
+   
+   var config = bracketsConfig.map(v => v.join(''));
+   
+   var count = 0;
+   var stop = str.length / 2;
+   
+   while(str.length > 1 && count < stop){  
+        for(i=0;i<config.length;i++){
+        str = str.replace(config[i],'')   
+        }
+    count++;
+   }
+   
+   return str == 0
+  
+  }  
